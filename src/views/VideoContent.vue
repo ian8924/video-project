771d32948bfbac8c983155dc2a4bda97abc12a47
Ian Liao <justforian0915@gmail.com>
2020-08-14 19:13:26 +0800

    [Modify] 播放夜增加顯示影片資訊

--- src/views/VideoContent.vue
+++ src/views/VideoContent.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="contain">
+    <div>
+      <h1>{{title}}</h1>
+    </div>
       <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" style="margin:0 auto">
           <source src="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8" type="application/x-mpegURL">
-        </video>
+      </video>
+      <div class="chaanel">
+        頻道：
+        <h3>{{channelTitle}}</h3>
+      </div>
+      <div class="description">
+      影片描述
+        <h3>{{description}}</h3>
+      </div>
   </div>
 </template>
 
@@ -14,7 +25,6 @@ export default {
   data () {
     return {
       vedioID: '',
-      imgURL: '',
       setting: {
         bigPlayButton: false,
         textTrackDisplay: false,
@@ -24,13 +34,21 @@ export default {
         poster: ''
       },
       windowWidth: window.innerWidth * 0.9,
-      player: null
+      player: null,
+      currentVedioInfo: {
+        snippet: {}
+      }
     }
   },
-  mounted () {
-    this.vedioID = this.$route.query.vedioID
-    this.imgURL = this.$route.query.imgUrl
-    this.setting.poster = this.imgURL
+  async mounted () {
+    this.$store.commit('clearVedioList')
+    await this.$store.dispatch('getVideoList')
+    this.vedioID = this.$route.query.id
+    this.currentVedioInfo = this.$store.state.videoList.find(item => {
+      return item.id === this.$route.query.id
+    })
+    // 開始預設IMG
+    this.setting.poster = this.currentVedioInfo.snippet.thumbnails.high.url
     window.addEventListener('resize', this.onResize)
     this.player = videojs('my-video', this.setting, () => {
     })
@@ -57,6 +75,17 @@ export default {
       }
     }
   },
+  computed: {
+    title () {
+      return this.currentVedioInfo.snippet.title || ''
+    },
+    channelTitle () {
+      return this.currentVedioInfo.snippet.channelTitle || ''
+    },
+    description () {
+      return this.currentVedioInfo.snippet.description || ''
+    }
+  },
   watch: {
     windowWidth () {
       this.setVedioSize()
@@ -64,3 +93,16 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.description{
+  margin:0 auto;
+  width: 80%;
+  margin-top: 20px;
+  color: gray;
+  white-space:pre-line;
+}
+.chaanel{
+  margin-top: 20px;
+  color: red;
+}
+</style>
